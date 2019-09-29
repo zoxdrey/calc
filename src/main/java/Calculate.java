d09f40e274d601a2595c489bec71fe839e52c808
Вячеслав Петров <zoxypva@gmail.com>
2019-09-29 23:03:25 +0300

    some changes

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -5,107 +6,187 @@ import java.util.Stack;
 public class  Calculate {
 
     private static Stack<String> stack = new Stack<>();
+    private static Stack<Integer> calcResult = new Stack<>();
     public static List<String> all = new ArrayList<>();
     public static List<String> numbers = new ArrayList<>();
     public static List<String> oper = new ArrayList<>();
-    public static  List<StringBuilder> result = new ArrayList<>();
-    private static List<String> operands = new ArrayList<>();
+    public static  List<String> result = new ArrayList<>();
+    private static List<String> exitString = new ArrayList<>();
+    public static int oper1;
+    public static int oper2;
+    public static String operation;
+    public static int calculationResult;
     public static String calc(String expression) throws Exception {
-
-        System.out.println(Parse(expression));
-        System.out.println(charComparison(all));
-        System.out.println(stack);
+        //System.out.println(Parse(expression));
+        CalcResult(result);
         return expression;
     }
 
+    static Integer CalcResult(List<String> result) {
+
+        for(int x = 0; x < result.size();x++) {
+            if(result.get(x) == "" ) continue;
+            switch (result.get(x))
+            {
+                case "+":
+                    oper1 = calcResult.pop();
+                    oper2 = calcResult.pop();
+                    calculationResult = oper1 + oper2;
+                    calcResult.push(calculationResult);
+                    System.out.println(calcResult);
+                    break;
+                case "-":
+                    oper1 = calcResult.pop();
+                    oper2 = calcResult.pop();
+                    calculationResult = oper1 - oper2;
+                    calcResult.push(calculationResult);
+                    System.out.println(calcResult);
+                    break;
+                case "*":
+                    oper1 = calcResult.pop();
+                    oper2 = calcResult.pop();
+                    calculationResult = oper1 * oper2;
+                    calcResult.push(calculationResult);
+                    System.out.println(calcResult);
+                    break;
+                case "/":
+                    oper1 = calcResult.pop();
+                    oper2 = calcResult.pop();
+                    calculationResult = oper2 / oper1;
+                    calcResult.push(calculationResult);
+                    System.out.println(calcResult);
+                    break;
+                default:
+                    calcResult.push(Integer.parseInt(result.get(x)));
+                    System.out.println(calcResult);
+            }
+        }
+        System.out.println(calcResult);
+
+        return calculationResult;
+    }
 
 
-
-    private static List<String> charComparison(List<String> Elements) throws Exception {
+    static List<String> charComparison(List<String> Elements) throws Exception {
         for (String element:Elements) {
             switch (element)
             {
             case "(":
+                if(stack.isEmpty()){
+                    stack.push("(");
+                    break;}
                 stack.push("(");
                 break;
-
             case ")":
-                stack.push(")");
+                if(stack.isEmpty()){
+                throw  new Exception("WTF");
+                }
+                while(!stack.peek().equals("(")){
+                    if(stack.isEmpty()){break;}
+                    exitString.add(stack.pop());
+                }
+                stack.pop();
                 break;
             case "+":
+                if(stack.isEmpty()){
+                    stack.push("+");
+                    break;}
+                if(GetPriority(stack.peek()) > 1) {
+                    exitString.add(stack.pop());
+                    stack.push("+");
+                    break;
+                }
+                else if(GetPriority(stack.peek()) == 1) {
+
+                    exitString.add(stack.pop());
+                    stack.push("+");
+                    break;
+                }
                 stack.push("+");
                 break;
             case "-":
+                if(stack.isEmpty()){
+                    stack.push("-");
+                    break;}
+                if(GetPriority(stack.peek()) > 1) {
+                    exitString.add(stack.pop());
+                    stack.push("-");
+                    break;
+                }
+                else if(GetPriority(stack.peek()) == 1) {
+                    exitString.add(stack.pop());
+                    stack.push("-");
+                    break;
+                }
                 stack.push("-");
                 break;
             case "*":
+                if(stack.isEmpty()){
+                    stack.push("*");
+                    break;}
+                if(GetPriority(stack.peek()) > 2) {
+                    exitString.add(stack.pop());
+                    stack.push("*");
+                    break;
+                }
+                else if(GetPriority(stack.peek()) == 2) {
+                    exitString.add(stack.pop());
+                    stack.push("*");
+                    break;
+                }
                 stack.push("*");
                 break;
             case "/":
+                if(stack.isEmpty()){
+                    stack.push("/");
+                    break;}
+                if(GetPriority(stack.peek()) > 2) {
+                    exitString.add(stack.pop());
+                    stack.push("/");
+                    break;
+                }
+                else if(GetPriority(stack.peek()) == 2) {
+                    exitString.add(stack.pop());
+                    stack.push("/");
+                    break;
+                }
                 stack.push("/");
                 break;
             case "^":
                 stack.push("^");
                 break;
             default:
-                operands.add(element);
+                exitString.add(element);
                 break;
         }
         }
-        return operands;
+        while(!stack.isEmpty()){
+        exitString.add(stack.pop());}
+        System.out.println(exitString);
+        return exitString;
     }
 
 
 
-    public byte GetPriority(char s)
+    public static byte GetPriority(String s)
     {
         switch (s)
         {
-            case '(':
-            case ')':
+            case "(":
+            case ")":
                 return 0;
-            case '+':
-            case '-':
+            case "+":
+            case "-":
                 return 1;
-            case '*':
-            case '/':
+            case "*":
+            case "/":
                 return 2;
-            case '^':
+            case "^":
                 return 3;
             default:
                 return 4;
         }
     }
 
-    static List<String> Parse(String expression)  {
-        GetNumbers(expression);
-        GetChars(expression);
-        for(int x=0; x<numbers.size(); x++){
-            all.add(numbers.get(x));
-            if(x != numbers.size()-1){
-                all.add(oper.get(x));}
-        }
-        return all;
-    }
 
-
-
-    static List<String>  GetNumbers(String str){
-        String replaceString = str.replaceAll("[^-?0-9]+", " ");
-        String[] replaceString1 = replaceString.split(" ");
-        for (String elem : replaceString1) {
-            numbers.add(elem);
-        }
-        return numbers;
-    }
-
-    static List<String>  GetChars(String str){
-        String replaceString = str.replaceAll("[-?0-9]+", " ");
-        String[] replaceString1 = replaceString.split(" ");
-        for (String elem : replaceString1) {
-            oper.add(elem);
-        }
-        oper.remove(0);
-        return oper;
-    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        String expression = in.next();
 
-        Calculate.calc(expression);
+    private static String tempo = "";
+    public static List<String> parsingString = new ArrayList<String>();
+    static int result;
+
+    public static void main(String[] args) throws Exception {
+        //Scanner in = new Scanner(System.in);
+        //System.out.print("Input a number: ");
+        //String expression = in.next();
+
+        //Calculate.calc("5*2+10");
+        Parse("53*(2+2)/10");
+        parsingString = Calculate.charComparison(parsingString);
+        result = Calculate.CalcResult(parsingString);
+        System.out.println(result+" - результат");
+    }
+
+    public static void Parse(String str)
+    {
+        System.out.println(str+" - Исходное выражение");
+        for(int x = 0; x <str.length();x++)
+        {
+            if(Character.isDigit(str.charAt(x))){
+                System.out.println(str.charAt(x)+" - Число");
+
+                tempo = tempo + str.charAt(x);
+                System.out.println(tempo+" - Число");
+                if(x==str.length()-1){
+                    parsingString.add(tempo);
+                }
+            }
+            else {
+
+                System.out.println(str.charAt(x)+" - Не число");
+                parsingString.add(tempo);
+                parsingString.add(String.valueOf(str.charAt(x)));
+
+                tempo = "";
+            }
+        }
+        System.out.println(parsingString+" - Выходной массив");
     }
 }
